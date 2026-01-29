d6849a95de733905c89a20d5b6df06eb0b12b30c
keblb1dk <156938228+keblb1dk@users.noreply.github.com>
2026-01-28 18:52:21 -0700

    Refactor PrimaryGeneratorAction header file
    Included new helper functions to accept file inputs from command line

--- include/PrimaryGeneratorAction.hh
+++ include/PrimaryGeneratorAction.hh
@@ -1,21 +1,52 @@
-#ifndef PRIMARYGENERATORACTION_HH
-#define PRIMARYGENERATORACTION_HH
-#include <fstream>
+#ifndef PrimaryGeneratorAction_h
+#define PrimaryGeneratorAction_h 1
 
 #include "G4VUserPrimaryGeneratorAction.hh"
+#include "G4ParticleGun.hh"
 #include "globals.hh"
+#include <fstream>
 
 class G4ParticleGun;
+class G4Event;
+class PrimaryGeneratorMessenger;
 
-class PrimaryGeneratorAction : public G4VUserPrimaryGeneratorAction {
+class PrimaryGeneratorAction : public G4VUserPrimaryGeneratorAction
+{
 public:
     PrimaryGeneratorAction();
     virtual ~PrimaryGeneratorAction();
 
     virtual void GeneratePrimaries(G4Event*);
+
+    const G4ParticleGun* GetParticleGun() const { return fParticleGun; }
+
+    // Methods to control energy sampling mode
+    void SetUseMeanSampling(G4bool val);
+    G4bool GetUseMeanSampling() const { return fUseMeanSampling; }
+
+    //MEthod to set muon energy threshold...default 0.0
+    void SetMuonEnergyThreshold(G4double eThreshold);
+    G4double GetMuonEnergyThreshold() const { return fEnergyThreshold; }
+
+
+    // Methods to set data file paths
+    void SetAngularDataFile(const G4String& filename);
+    void SetMeanEnergyFile(const G4String& filename);
+    void SetCDFEnergyFile(const G4String& filename);
+
 private:
     G4ParticleGun* fParticleGun;
     std::ofstream fTotalAngles;
+    PrimaryGeneratorMessenger* fMessenger;
+
+    // Control flag for energy sampling mode
+    G4bool fUseMeanSampling;  // true = mean energy, false = CDF sampling
+    G4double fEnergyThreshold;
+
+    // File paths
+    G4String fAngularDataFile;
+    G4String fMeanEnergyFile;
+    G4String fCDFEnergyFile;
 };
 
 #endif
